bfb35f0657a23b4f8d42c45dbb622a6b6cfb7874
Johanna Liu <johanna.liu@mths.ca>
2022-10-13 15:16:26 +0000

    init

---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna Liu
+# Created on: Oct 2022
+# This file holds the constants for the guessing program
+
+SPECIAL_NUMBER = 5
---
+++ guessing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna Liu
+# Created on: Oct 2022
+# This program adds two numbers
+
+import constants
+
+
+def main():
+    # this is a number guessing game
+
+    # input
+    print("Number Guessing Game")
+    user_number = int(input("Enter a number between 0-9: "))
+
+    # process
+    if user_number == constants.SPECIAL_NUMBER:
+        print("YAY! You guessed the correct number!")
+
+    if user_number != constants.SPECIAL_NUMBER:
+        print("Try again.")
+
+    # output
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
